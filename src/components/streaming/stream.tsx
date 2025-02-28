035c3f394850bb27b7dc42cba38bc56faefc2885
FabriceRandrianaivo <fabricerandrianaivo8@gmail.com>
2025-02-28 22:35:02 +0300

    modification nom projet

--- src/components/streaming/stream.tsx
+++ src/components/streaming/stream.tsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const VideoStream = () => {
-    const [isPlayStream, setIsPlayStream] = useState<boolean>(false);
-    return (
-        <>
-            <div>
-                <h2>Live Stream</h2>
-                <img src="http://localhost:8000/stream/video_feed" alt="Video Stream" width="640" />
-            </div>
-        </>
-    );
+const VideoStream: React.FC = () => {
+  const [imageSrc, setImageSrc] = useState<string>("");
+
+  useEffect(() => {
+    const fetchFrame = async () => {
+      try {
+        const response = await fetch("http://localhost:8000/streal/video_feed", {
+          headers: {
+            Authorization: "Bearer mon_secret_token", // Remplace avec ton vrai token
+          },
+        });
+
+        if (response.ok) {
+          setImageSrc("http://localhost:8000/stream/video_feed");
+        } else {
+          console.error("Requête non autorisée");
+        }
+      } catch (error) {
+        console.error("Erreur de connexion au serveur:", error);
+      }
+    };
+
+    fetchFrame();
+  }, []);
+
+  return (
+    <div>
+      <h2>Détection d'objets avec YOLO</h2>
+      {imageSrc ? (
+        <img src={imageSrc} alt="Video Stream" width="640" />
+      ) : (
+        <p>Chargement du flux...</p>
+      )}
+    </div>
+  );
 };
 
 export default VideoStream;
